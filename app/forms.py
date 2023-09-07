5785261d1d2c9f41c2eaf1c71bc3777254ca174b
prasanth <prasanthsenthilkumaran@gmail.com>
2023-09-07 21:02:33 +0530

    hi

--- app/forms.py
+++ app/forms.py
@@ -3,3 +3,15 @@ from django import forms
 class Studentforms(forms.Form):
     sname = forms.CharField(max_length=100)
     sid = forms.IntegerField()
+    smark = forms.FloatField()
+    spercentage = forms.FloatField()
+    sdob = forms.DateField()
+    semail = forms.EmailField()
+    Sclasstime = forms.TimeField()
+    Surl=forms.URLField()
+    sdrop = forms.ChoiceField()
+    sdroplist= forms.CharField(widget= forms.PasswordInput)
+    stext = forms.CharField(widget=forms.Textarea)
+    schoicefiles= forms.ChoiceField(widget=forms.RadioSelect)
+    smulch= forms.MultipleChoiceField(widget=forms.CheckboxInput)
+    # sslug= forms.SlugField() # invalid
